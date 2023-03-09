39c9a8afb06d2d0012c4029357281463e28fdbc9
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-03-09 14:46:21 +0300

    add footer component to app.vue

--- product-website/src/App.vue
+++ product-website/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import MainNav from './components/MainNav.vue'
+import FooterSection from './components/FooterSection.vue'
 </script>
 
 <template>
   <MainNav />
+  <FooterSection />
 </template>
