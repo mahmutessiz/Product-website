ca01c9e24fae6c26b8113f6d68ef30950da0b667
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-03-09 14:41:46 +0300

    header component & drawer created

--- product-website/src/App.vue
+++ product-website/src/App.vue
@@ -1,21 +1,7 @@
-<script setup></script>
+<script setup>
+import MainNav from './components/MainNav.vue'
+</script>
 
 <template>
-  <h1>hi</h1>
-
-  <div class="drawer">
-    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content">
-      <!-- Page content here -->
-      <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
-    </div>
-    <div class="drawer-side">
-      <label for="my-drawer" class="drawer-overlay"></label>
-      <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-        <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-      </ul>
-    </div>
-  </div>
+  <MainNav />
 </template>
---
+++ product-website/src/components/MainNav.vue
@@ -0,0 +1,48 @@
+<script setup></script>
+<template>
+  <header class="flex w-full items-center justify-between gap-4 bg-black py-6 px-4 sm:px-12">
+    <nav class="flex gap-4">
+      <img src="../../public/favicon.ico" alt="logo" />
+      <div class="hidden md:flex">
+        <ul class="flex gap-4 text-3xl text-white">
+          <li><a>Docs</a></li>
+          <li><a>Showcase</a></li>
+          <li><a>Resouces</a></li>
+          <li><a>About</a></li>
+          <li><a>Blog</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="flex items-center justify-center gap-4 text-3xl">
+      <a class="hidden sm:flex">T</a>
+      <a class="hidden sm:flex">D</a>
+      <a class="hidden sm:flex">G</a>
+      <div class="drawer-content md:hidden">
+        <!-- Page content here -->
+        <label for="my-drawer" class="btn-primary btn-outline drawer-button btn">&#9776;</label>
+      </div>
+    </div>
+  </header>
+
+  <div class="drawer fixed z-50 md:hidden">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+
+    <div class="drawer-side">
+      <label for="my-drawer" class="drawer-overlay"></label>
+      <ul class="menu w-full gap-4 bg-base-100 p-4 font-mono text-3xl text-white">
+        <!-- Sidebar content here -->
+        <li><a>Docs</a></li>
+        <li><a>Showcase</a></li>
+        <li><a>Resouces</a></li>
+        <li><a>About</a></li>
+        <li><a>Blog</a></li>
+        <li class="flex flex-row justify-evenly">
+          <a>T</a>
+          <a>D</a>
+          <a>G</a>
+        </li>
+        <button class="btn-outline btn-secondary btn">Get Started</button>
+      </ul>
+    </div>
+  </div>
+</template>
