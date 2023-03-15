7cc64bff38daabd1e5a7e2682b701793f97c6c10
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-03-15 17:29:21 +0300

    make video bg avalable to phones

--- product-website/src/components/MainSection.vue
+++ product-website/src/components/MainSection.vue
@@ -13,12 +13,24 @@ import GetInTouchSection from './GetInTouchSection.vue'
       muted
       loop
       poster="../images/facebook.png"
+      class="absolute top-0 w-full opacity-20 md:hidden"
+    >
+      <source src="../images/phone_bg.webm" type="video/webm" />
+      Your browser does not support the video tag.
+    </video>
+
+    <video
+      playsinline
+      autoplay
+      muted
+      loop
+      poster="../images/facebook.png"
       class="absolute -z-10 hidden w-full md:block"
     >
       <source src="../images/site_bg.webm" type="video/webm" />
       Your browser does not support the video tag.
     </video>
-    <div class="md:w-1/2">
+    <div class="z-10 md:w-1/2">
       <h1 class="text-center font-serif text-3xl font-bold leading-snug text-gray-200 sm:text-6xl">
         <span class="bg-gradient-to-r from-rose-400 to-purple-500 bg-clip-text text-transparent">
           Add your website's motto in here
@@ -30,7 +42,7 @@ import GetInTouchSection from './GetInTouchSection.vue'
         placeat quae voluptatum eos officiis aspernatur!
       </p>
       <div class="flex w-full justify-center">
-        <button class="btn-warning btn-outline btn mt-9 hover:scale-105">Try for free</button>
+        <button class="btn-outline btn-warning btn mt-9 hover:scale-105">Try for free</button>
       </div>
     </div>
   </main>
