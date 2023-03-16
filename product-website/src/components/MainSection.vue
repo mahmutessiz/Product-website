e65a001be3434382fa55569e3d882b0921265de7
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-03-16 14:08:36 +0300

    change sponsorSection position

--- product-website/src/components/MainSection.vue
+++ product-website/src/components/MainSection.vue
@@ -4,6 +4,7 @@ import CompareOthersSection from './CompareOthersSection.vue'
 import UtilitiesSection from './UtilitiesSection.vue'
 import GetInTouchSection from './GetInTouchSection.vue'
 import TestimonialSection from './TestimonialSection.vue'
+import SponsorSection from './SponsorSection.vue'
 </script>
 
 <template>
@@ -37,7 +38,7 @@ import TestimonialSection from './TestimonialSection.vue'
           Add your website's motto in here
         </span>
       </h1>
-      <p class="mt-12 text-center text-gray-300 sm:text-xl">
+      <p class="mt-12 text-left text-gray-300 sm:pl-6 sm:text-xl">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, consequatur at. Soluta
         laboriosam qui eaque cumque cum ipsa quibusdam nostrum expedita aliquam iste assumenda,
         placeat quae voluptatum eos officiis aspernatur!
@@ -47,53 +48,11 @@ import TestimonialSection from './TestimonialSection.vue'
       </div>
     </div>
   </main>
-  <div class="m-auto mt-12 w-2/3 max-w-[52rem] text-center" id="sponsor-div">
-    <span class="text-2xl text-gray-200"
-      >Sponsors
-      <hr />
-    </span>
-    <ul class="mt-4 flex flex-wrap justify-center gap-6 p-9">
-      <li class="border-b border-warning shadow-sm shadow-slate-500">
-        <img
-          src="https://picsum.photos/100/60/"
-          class="transition-transform duration-300 hover:scale-105"
-          alt=""
-        />
-      </li>
-      <li class="border-b border-warning shadow-sm shadow-slate-500">
-        <img
-          src="https://picsum.photos/100/64/"
-          class="transition-transform duration-300 hover:scale-105"
-          alt=""
-        />
-      </li>
-      <li class="border-b border-warning shadow-sm shadow-slate-500">
-        <img
-          src="https://picsum.photos/100/63/"
-          class="transition-transform duration-300 hover:scale-105"
-          alt=""
-        />
-      </li>
-      <li class="border-b border-warning shadow-sm shadow-slate-500">
-        <img
-          src="https://picsum.photos/100/62/"
-          class="transition-transform duration-300 hover:scale-105"
-          alt=""
-        />
-      </li>
-      <li class="border-b border-warning shadow-sm shadow-slate-500">
-        <img
-          src="https://picsum.photos/100/61/"
-          class="transition-transform duration-300 hover:scale-105"
-          alt=""
-        />
-      </li>
-    </ul>
-  </div>
 
   <ProductsSection />
   <UtilitiesSection />
   <CompareOthersSection />
   <TestimonialSection />
+  <SponsorSection />
   <GetInTouchSection />
 </template>
---
+++ product-website/src/components/SponsorSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="m-auto mt-12 w-2/3 max-w-[52rem] text-center" id="sponsor-div">
+    <span class="text-2xl text-gray-200">Sponsors </span>
+    <ul class="mt-4 flex flex-wrap justify-center gap-6 p-9">
+      <li class="border-b border-warning shadow-sm shadow-slate-500">
+        <img
+          src="https://picsum.photos/100/60/"
+          class="transition-transform duration-300 hover:scale-105"
+          alt=""
+        />
+      </li>
+      <li class="border-b border-warning shadow-sm shadow-slate-500">
+        <img
+          src="https://picsum.photos/100/64/"
+          class="transition-transform duration-300 hover:scale-105"
+          alt=""
+        />
+      </li>
+      <li class="border-b border-warning shadow-sm shadow-slate-500">
+        <img
+          src="https://picsum.photos/100/63/"
+          class="transition-transform duration-300 hover:scale-105"
+          alt=""
+        />
+      </li>
+      <li class="border-b border-warning shadow-sm shadow-slate-500">
+        <img
+          src="https://picsum.photos/100/62/"
+          class="transition-transform duration-300 hover:scale-105"
+          alt=""
+        />
+      </li>
+      <li class="border-b border-warning shadow-sm shadow-slate-500">
+        <img
+          src="https://picsum.photos/100/61/"
+          class="transition-transform duration-300 hover:scale-105"
+          alt=""
+        />
+      </li>
+    </ul>
+  </div>
+</template>
